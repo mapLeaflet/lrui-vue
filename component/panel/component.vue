0045aaf407f9ad2d375d40df6ec7fd45c3f4b86c
liaohui1080 <liaohui1080@tianliaohui.com>
2017-08-31 10:52:26 +0800

    panel 面板完成

--- component/panel/component.vue
+++ component/panel/component.vue
@@ -1,19 +1,53 @@
 <script>
     module.exports = {
-        props: {},
+        props: {
+            auto: Boolean
+        },
 
 
         data: function () {
-            return {}
+            return {
+
+                bodyHeight: 0
+
+
+            }
         },
 
 
         mounted: function () {
 
 
-        },
+            var self = this;
 
-        created: function (e) {
+
+            //控制中间内容区域,自适应屏幕高度
+            function bodyResize() {
+                var windowHeight = window.innerHeight;
+                var headerHeight = self.$refs.header.offsetHeight;
+                var footHeight = self.$refs.foot.offsetHeight;
+
+
+                var bodyHeight = windowHeight - headerHeight - footHeight - 2;
+
+
+                self.$refs.body.style.height = String(bodyHeight + 'px');
+            }
+
+
+
+            //如果 auto 存在, 就启用 body区域自适应屏幕
+            if (this.auto) {
+
+
+                window.onresize = function () {
+                    bodyResize()
+                };
+
+                bodyResize();
+
+            }
+
 
         },
 
@@ -30,13 +64,16 @@
 
         <div class="panel panel-default">
 
+            <div ref="header">
+                <slot name="header">啊上的发上的</slot>
+            </div>
 
-            <slot name="header">啊上的发上的</slot>
+            <div ref="body" style="overflow-y: auto">
 
+                <slot></slot>
+            </div>
 
-            <slot></slot>
-
-            <div class="panel-body" style="background: #20d8f0">
+            <div ref="foot">
                 <slot name="foot"></slot>
             </div>
 
@@ -50,6 +87,10 @@
     /*css 注意css 只作用域当前组件, 在写的时候注意名字 不要和其他组件重复, 推荐只用行内css*/
     .lr-panel {
 
+    }
+
+    .lr-panel .panel-default {
+        margin: 0;
     }
 
     .lr-panel .panel-body {
